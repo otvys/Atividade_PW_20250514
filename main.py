08051d707cfb2629d57a52260a19b466ba66aa90
Otavia <otaviamonteiro09@gmail.com>
2025-05-14 15:06:32 -0300

    Primeira etapa concluída

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ async def get_clientes():
     response = templates.TemplateResponse("clientes.html", {"request": {}, "clientes": clientes})
     return response
 
+@app.get("/produtos")
+async def get_produtos():
+    produtos = produto_repo.obter_todos()
+    response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
+    return response
+
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
